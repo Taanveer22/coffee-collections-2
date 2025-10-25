ab6e16c804e20f689cc21fa717f95b9677bd1435
Taanveer22 <taanveer469@gmail.com>
2025-10-26 02:07:32 +0600

    alhamdulliah coffees data load from local storage to dasboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Heading from "../components/Heading";
+import { getFromFavourite } from "../utilities";
+import Card from "../components/Card";
 
 const Dashboard = () => {
+  const [coffees, setCoffees] = useState([]);
+  useEffect(() => {
+    const favouriteList = getFromFavourite();
+    // console.log(favouriteList);
+    setCoffees(favouriteList);
+  }, []);
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
+      <div className="mb-12">
+        <Heading
+          title="Welcome to Dashboard"
+          subTitle="Manage coffees that you have previously added as favorite. You can view or remove them from here."
+        ></Heading>
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          {coffees.map((coffeeItem) => (
+            <Card key={coffeeItem.id} coffeeItem={coffeeItem}></Card>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
